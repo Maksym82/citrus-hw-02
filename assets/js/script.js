2f39c8f507b7ef272f8bcf6d81ec87314dcc1a97
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-04-03 17:35:09 +0300

    Update styles fashion

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,12 @@ const swiper = new Swiper(".fashion-swiper", {
   breakpoints: {
     // when window width is >= 768px
     768: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20,
     },
     // when window width is >= 1280px
     1280: {
-      slidesPerView: 3,
+      slidesPerView: 2,
       spaceBetween: 20,
     },
     // when window width is >= 1440px
