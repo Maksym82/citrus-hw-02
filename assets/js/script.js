99eeda0c73faaac61b74554d97f3e4c62319ef40
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-04-03 16:26:00 +0300

    Fix errors

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper(".fashion-swiper", {
+  loop: true,
+  mousewheel: false,
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1280px
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1440px
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
